c16b127061d156d900cb43932243cdd5d43959b3
kioku <gkioku@gmail.com>
2019-10-15 15:51:39 +0900

    update

--- main.cpp
+++ main.cpp
@@ -12,6 +12,24 @@ mat4x4 worldMat;
 mat4x4 projMat;
 int viewport[4];
 
+const float INFINITY_T = 1.e10f;
+
+static const struct
+{
+    vec3 pos;
+    vec2 uv;
+} vertices[4] =
+{
+    { {-0.6f, -0.4f, 0.f}, {0.f, 0.f} },
+    { { 0.6f, -0.4f, 0.f}, {1.f, 0.f} },
+    { {-0.6f,  0.4f, 0.f}, {0.f, 1.f} },
+    { { 0.6f,  0.4f, 0.f}, {1.f, 1.f} }
+};
+int indices[2][3] = {
+    {0,1,2},
+    {2,1,3}
+};
+
 int glhUnProjectf(float winx, float winy, float winz, mat4x4 view, mat4x4 projection, int *viewport, float *objectCoordinate)
 {
     // Transformation matrices
@@ -39,37 +57,115 @@ int glhUnProjectf(float winx, float winy, float winz, mat4x4 view, mat4x4 projec
     return 1;
 }
 
+float intersectTriangle(vec3 orig, vec3 dir, vec4 vertices[3], float lastHitT, vec2 coord)
+{
+    vec3 u, v, n; // triangle vectors
+    vec3 w0, w;  // ray vectors
+    float r, a, b; // params to calc ray-plane intersect
+    
+    // get triangle edge vectors and plane normal
+    vec3_sub(u, vertices[1], vertices[0]);
+    vec3_sub(v, vertices[2], vertices[0]);
+    vec3_mul_cross(n, u, v);
+    
+    vec3_sub(w0, orig, vertices[0]);
+    a = -vec3_mul_inner(n, w0);
+    b = vec3_mul_inner(n, dir);
+    if (abs(b) < 1e-5)
+    {
+        // ray is parallel to triangle plane, and thus can never intersect.
+        return INFINITY_T;
+    }
+    
+    // get intersect point of ray with triangle plane
+    r = a / b;
+    if (r < 0.0)
+        return INFINITY_T; // ray goes away from triangle.
+    
+    vec3 I, rdir;
+    vec3_scale(rdir, dir, r);
+    vec3_add(I,orig, rdir);
+    float uu, uv, vv, wu, wv, D;
+    uu = vec3_mul_inner(u, u);
+    uv = vec3_mul_inner(u, v);
+    vv = vec3_mul_inner(v, v);
+    vec3_sub(w, I, vertices[0]);
+    wu = vec3_mul_inner(w, u);
+    wv = vec3_mul_inner(w, v);
+    D = uv * uv - uu * vv;
+    
+    // get and test parametric coords
+    float s, t;
+    s = (uv * wv - vv * wu) / D;
+    if (s < 0.0 || s > 1.0)
+        return INFINITY_T;
+    t = (uv * wu - uu * wv) / D;
+    if (t < 0.0 || (s + t) > 1.0)
+        return INFINITY_T;
+    
+    coord[0] = s;
+    coord[1] = t;
+    
+    return (r > 1e-5 || r < lastHitT) ? r : INFINITY_T;
+}
+
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
     //fprintf(stdout, "Pos: (%lf,%lf)\n", xpos, ypos);
     
-    // calc screen ray dir
-    vec3 startpos, endpos, raydir;
+    // calc view space ray dir
+    vec3 orgpos, endpos, raydir;
     float screenCoord[2] = {(float)xpos, (float)viewport[3] - (float)ypos};
-    glhUnProjectf(screenCoord[0], screenCoord[1], -1.f, viewMat, projMat, viewport, startpos); // on near plane
+    glhUnProjectf(screenCoord[0], screenCoord[1], -1.f, viewMat, projMat, viewport, orgpos); // on near plane
     glhUnProjectf(screenCoord[0], screenCoord[1],  1.f, viewMat, projMat, viewport, endpos);   // on far plane
-    vec3_sub(raydir, endpos, startpos);
-    fprintf(stdout, "s: (%f,%f,%f)\n", raydir[0], raydir[1], raydir[2]);
+    vec3_sub(raydir, endpos, orgpos);
+    //fprintf(stdout, "s: (%f,%f,%f)\n", raydir[0], raydir[1], raydir[2]);
     
+    float lastT = INFINITY_T;
+    int hitIndex = -1;
+    vec2 hitCoord;
+    for (int t = 0; t < 2; ++t) {
+        // calc view space triangles
+        vec4 viewtri[3];
+        vec4 worldtri[3];
+        for (int i = 0; i < 3; ++i) {
+            worldtri[i][0] = vertices[indices[t][i]].pos[0];
+            worldtri[i][1] = vertices[indices[t][i]].pos[1];
+            worldtri[i][2] = vertices[indices[t][i]].pos[2];
+            worldtri[i][3] = 1.0;
+            mat4x4_mul_vec4(viewtri[i], worldMat, worldtri[i]);
+        }
+        
+        // intersect ray and triangle
+        vec2 coord; // barycentric coordinates
+        float hitT = intersectTriangle(orgpos, raydir, viewtri, lastT, coord);
+        if (hitT < INFINITY_T) {
+            hitIndex = t;
+            hitCoord[0] = coord[0];
+            hitCoord[1] = coord[1];
+        }
+    }
     
-    // intersect ray and triangle
+    if (hitIndex != -1) {
+        //fprintf(stdout, "Hit Index=%d coord=(%f,%f)\n", hitIndex, hitCoord[0], hitCoord[1]);
+        
+        // calc UV from barycentric coordinates
+        vec2 v0, v1, uv0, uv1, uv;
+        vec2_sub(v0, vertices[indices[hitIndex][1]].uv,vertices[indices[hitIndex][0]].uv);
+        vec2_sub(v1, vertices[indices[hitIndex][2]].uv,vertices[indices[hitIndex][0]].uv);
+        vec2_scale(uv0, v0, hitCoord[0]);
+        vec2_scale(uv1, v1, hitCoord[1]);
+        vec2_add(uv, uv0, uv1);
+        vec2_add(uv, uv, vertices[indices[hitIndex][0]].uv);
+        
+        fprintf(stdout, "Hit Index=%d UV=(%f,%f)\n", hitIndex, uv[0], uv[1]);
+    }
     
     
 }
 
 //--------------------------
 
-static const struct
-{
-    float x, y, z;
-    float u, v;
-} vertices[4] =
-{
-    { -0.6f, -0.4f, 0.f,  0.f, 0.f },
-    {  0.6f, -0.4f, 0.f,  1.f, 0.f },
-    { -0.6f,  0.4f, 0.f,  0.f, 1.f },
-    {  0.6f,  0.4f, 0.f,  1.f, 1.f }
-};
 static const char* vertex_shader_text =
 "#version 110\n"
 "uniform mat4 world;\n"
@@ -207,8 +303,9 @@ int main(void)
         glViewport(viewport[0],viewport[1],viewport[2],viewport[3]);
         glClear(GL_COLOR_BUFFER_BIT);
         mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime() * 0.5);
-        mat4x4_translate(v, 0.f, 0.f, -5.f);
+        mat4x4_rotate_Z(m, m, (float) glfwGetTime() * 0.1);
+        mat4x4_rotate_Y(m, m, (float) glfwGetTime() * 0.2);
+        mat4x4_translate(v, 0.f, 0.f, -2.f);
         mat4x4_perspective(p, 60.0/180.f*M_PI, ratio, 0.1, 10.0f);
         mat4x4_mul(mv, v, m);
         mat4x4_mul(mvp, p, mv);
